b3a43ad89559387930b64ed4b99b41cd8a458301
Serkandgn81 <doganserkan344@gmail.com>
2024-08-20 09:32:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
 
 AUTO_LEAVE_ASSISTANT_TIME = int(
 
-    getenv("ASSISTANT_LEAVE_TIME", "5400")
+    getenv("ASSISTANT_LEAVE_TIME", "999999999999")
 
 )  # Remember to give value in Seconds
 
